5da993dc308a016505243644dd78a3892c116af7
Tristram Oaten <tristram@oaten.name>
2020-10-12 14:49:45 +0100

    thanks Alex! wrapped the store in RwLock

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,29 @@ use std::env;
 use rocket::http::RawStr;
 use rocket::State;
 use std::collections::HashMap;
+use std::sync::RwLock;
 
-struct Store<'a> {
-    store: HashMap<& 'a String, String>,
+struct Store<> {
+    store: RwLock<HashMap<String, String>>,
 }
 
 #[get("/store/<id>")]
 fn retrieve(id: &RawStr, store: State<Store>) -> String {
-    format!("{}: {:?}", id, store.store.get(&id.to_string()))
+    let _astring = "test";
+    let hm = store.store.read().unwrap();
+    format!("{}: {:?}", id, hm.get(&id.to_string()))
 }
 
 #[get("/store/<id>/<val>")]
 fn set(id: &RawStr, val: &RawStr, store: State<Store>) -> String {
     let id_copy = id.to_string();
     let value = val.to_string();
-    store.store.insert(&id_copy, value);
-    format!("{}: {:?}", id, store.store.get(&id.to_string()))
+
+    let mut hm = store.store.write().unwrap();
+    hm.insert(id_copy, value);
+    drop(hm);
+    let hm_read = store.store.read().unwrap();
+    format!("{}: {:?}", id, hm_read.get(&id.to_string()))
 }
 
 fn main() {
@@ -29,7 +36,7 @@ fn main() {
         Err(e) => println!("Error: {}", e),
     }
     rocket::ignite()
-        .manage(Store { store: HashMap::new()})
-        .mount("/", routes![retrieve])
+        .manage(Store { store: RwLock::new(HashMap::new())})
+        .mount("/", routes![retrieve, set])
         .launch();
 }
