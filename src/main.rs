463bc701799b1f074b65a44fc558e45c4f460723
Tristram Oaten <tristram@oaten.name>
2020-11-16 16:40:36 +0000

    1/2 way through getting rocket async

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,37 @@
+#!/usr/bin/env run-cargo-script
+//! ```cargo
+//! [dependencies]
+//! rocket = "0.4.4"
+//! ```
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
 
 use std::env;
+use mini_redis::{client, Result};
 use rocket::http::RawStr;
-use rocket::State;
-use std::collections::HashMap;
-use std::sync::RwLock;
-
-struct Store<> {
-    store: RwLock<HashMap<String, String>>,
-}
 
 #[get("/store/<id>")]
-fn retrieve(id: &RawStr, store: State<Store>) -> String {
-    let _astring = "test";
-    let hm = store.store.read().unwrap();
-    format!("{}: {:?}", id, hm.get(&id.to_string()))
+async fn retrieve(id: &RawStr) -> Result<String> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+    let result = client.get(&id.to_string()).await?;
+    Ok(format!("{}", id))
 }
 
 #[get("/store/<id>/<val>")]
-fn set(id: &RawStr, val: &RawStr, store: State<Store>) -> String {
-    let id_copy = id.to_string();
-    let value = val.to_string();
+fn set(id: &RawStr, val: &RawStr) -> String {
+    let value= val.to_string();
 
-    let mut hm = store.store.write().unwrap();
-    hm.insert(id_copy, value);
-    drop(hm);
-    let hm_read = store.store.read().unwrap();
-    format!("{}: {:?}", id, hm_read.get(&id.to_string()))
+    format!("{}: {:?}", id, value)
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     match env::var("PORT") {
         Ok(val) => env::set_var("ROCKET_PORT", val),
         Err(e) => println!("Error: {}", e),
     }
     rocket::ignite()
-        .manage(Store { store: RwLock::new(HashMap::new())})
         .mount("/", routes![retrieve, set])
         .launch();
+    Ok(())
 }
